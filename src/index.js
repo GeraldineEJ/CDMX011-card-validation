28fdc2514256e22ddb014222b0cd549b80902151
MacBook Air <macbookair@MacBook-Air-de-Geraldine.local>
2021-07-20 12:14:37 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+// Lo que hace el botón validar
+document.getElementById('btnValidar').addEventListener('click',mostrar);
+
+
+
+// Función para mostrar los datos ingresados 
+
+function mostrar() {
+    
+const digitsCard = document.getElementById('numTarjeta').value;
+const valid =validator.isValid(digitsCard)
+const ocultar =validator.maskify(digitsCard)
+document.getElementById("hiden").innerHTML = ocultar;
+const mostrarDigitos= digitsCard;
+document.getElementById("mostrarM").innerHTML = mostrarDigitos;
+
+
+if(digitsCard==""){
+    alert("Ingrese un número de tarjeta")
+    }
+else if (valid===true){
+
+document.getElementById("div1").style.display = "none";
+document.getElementById("message").innerHTML = 'Tu tarjeta es válida';
+document.getElementById("btnRegresar").style.display = "block";
+document.getElementById("hiden").style.display = "block";
+document.getElementById("mostrarM").style.display = "none";
+    }
+
+else {
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("message").innerHTML = 'Tu tarjeta es inválida';
+    document.getElementById("btnRegresar").style.display = "block";
+    document.getElementById("hiden").style.display = "none";
+    document.getElementById("mostrarM").style.display = "block";
+    }   
+
+}
+
+// Lo que hace el botón regresar
+document.getElementById("btnRegresar").addEventListener('click', regresar );
+
+function regresar() {
+
+    document.getElementById("div1").style.display = "block";
+    document.getElementById("numTarjeta").value = "";
+    document.getElementById("btnRegresar").style.display = "none";
+    document.getElementById("message").style.display = "none";
+    document.getElementById("hiden").style.display = "none";
+    document.getElementById("mostrarM").style.display = "none";   
+    
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,90 @@
 const validator = {
-  // ...
+  
+ isValid: (digitsCard) => {
+    
+
+    let pares;
+    let sumaTotal=0;
+    
+    //Completar digitos si el número ingresado es menor de <16 digitos
+ if (digitsCard.length < 16) {
+    digitsCard="0".repeat(16-digitsCard.length)+digitsCard;
+ //console.log(digitsCard);
+}
+
+// Convierto en Array los dígitos que ingresa el usuario
+   
+ let arrayDigits= Array.from(digitsCard);
+ //console.log(arrayDigits);
+ 
+   
+
+    // Invierto el array
+
+let arrayReverse= arrayDigits.reverse();
+//console.log(arrayReverse);
+
+    //Lo convierto en numero
+for(let i=0; i<arrayReverse.length; i++) {
+    arrayReverse[i]= Number(arrayReverse[i]);
+   
+}
+
+    
+    // Duplica los numeros que están en posición par
+
+for (let i=1; i<arrayReverse.length; i+=2) {
+   
+    pares=arrayReverse[i]*2;
+       // console.log(pares);
+    
+    
+
+    // Suma los digitos de los números >9
+
+if (pares>9) {
+    pares = Math.trunc(pares / 10) + (pares % 10);
+    console.log("suma digitos del numero "+pares);
+    
+ }
+
+
+    //Suma todos los números
+    sumaTotal=sumaTotal+arrayReverse[i-1]+pares;
+    console.log("suma total es de "+ sumaTotal);
+
+}
+
+
+
+// Validar la tarjeta
+if (sumaTotal%10 ==0) {
+    console.log("tarjeta valida");
+    return true;
+}
+
+else {
+    console.log("Tarjeta invalida");
+    return false; 
+} 
+
+
+
+} 
+ 
+,maskify : (arrayDigits) =>  {
+        if (arrayDigits.length>4) {
+        arrayDigits="#".repeat(arrayDigits.length-4) + arrayDigits.slice(-4);
+        console.log(arrayDigits);
+
+        }
+    return arrayDigits;
+}
+
+
+
+
+
 };
 
 export default validator;
